04b38627efe23839bd063b8f3cebc744778948b5
Shinya Hara <s.hara@sonicmoov.com>
2023-11-11 12:04:34 +0900

    ✅ Add test for findAll method in TasksController

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { Task } from '@prisma/client';
 
 describe('TasksController', () => {
   let controller: TasksController;
+  let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
+      providers: [TasksService],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
+    service = module.get<TasksService>(TasksService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAll', async () => {
+    const result: Task[] = [
+      {
+        id: 1,
+        title: 'title',
+        content: 'content',
+        userId: 1,
+        deletedAt: null,
+        completedAt: null,
+      },
+    ];
+    jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+
+    expect(await controller.findAll()).toBe(result);
   });
 });
